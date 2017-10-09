060999c5d60f689e7578cd94f6dd522d912e245e
Tien Tran <tientran695@gmail.com>
2017-10-09 21:51:44 +0700

    make it better

--- index.js
+++ index.js
@@ -20,51 +20,7 @@ appServer.get('/', function (req, res) {
 //---------
 appServer.post('/', function (req, res) {
   console.log("Nhan ban tin POST");
-
-  var request = app.voiceRequest(options);
-  request.on('response', function(response) {
-    console.log(response);
-});
-
-  request.on('error', function(error) {
-    console.log(error);
-});
-
-request.end();
+  console.log(req.body.result);
 })
 
 server.listen(process.env.PORT || PORT);
-
-
-// var server = http.createServer(function(request, response) {
-//     if (request.method = 'POST' && request.url == '/upload') {
-//         // var outStream = fs.createWriteStream('qwe.wav');
-//         var voiceRequest = app.voiceRequest();
-//
-//         voiceRequest.on('response', function(_response) {
-//             response.end(JSON.stringify(_response));
-//             // var json = JSON.stringify({'resolvedQuery': _response['result']['resolvedQuery']})
-//             // response.end(json);
-//         });
-//
-//         voiceRequest.on('error', function(error) {
-//             console.log(error);
-//             response.end();
-//         });
-//
-//         request.on('data', function(chunk) {
-//             voiceRequest.write(chunk);
-//         });
-//
-//         request.on('end', function() {
-//             voiceRequest.end();
-//         });
-//     } else {
-//         response.writeHead(code, {});
-//         response.end();
-//     }
-//
-//     console.log(request.headers);
-// });
-
-// cat ann_smith.wav | curl -v -X POST --data-binary @- -H "Transfer-Encoding: chunked" -H "Content-Type: audio/wav" http://localhost:8000/upload
