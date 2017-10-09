abd3463f5694fb244e4811d3d668c0f28f2b6780
Tien Tran <tientran695@gmail.com>
2017-10-09 20:38:15 +0700

    make it better

--- index.js
+++ index.js
@@ -4,43 +4,54 @@
  * Apache 2.0 Licensed
  */
 const PORT = 3484;
-var http = require('http');
-// var apiai = require("../module/apiai");
+//var http = require('http');
+var express = require('express');
+var appServer = express();
+//var server = http.Server(appServer);
 var apiai = require("apiai")
+var appJarvis = apiai("33371b1bd65149429018582c7b7f8b6b");
 
-var app = apiai("33371b1bd65149429018582c7b7f8b6b");
+appServer.get('/', function (req, res) {
+  console.log("Nhan ban tin GET");
+})
 
-var server = http.createServer(function(request, response) {
-    if (request.method = 'POST' && request.url == '/upload') {
-        // var outStream = fs.createWriteStream('qwe.wav');
-        var voiceRequest = app.voiceRequest();
+appServer.post('/', function (req, res) {
+  console.log("Nhan ban tin POST");
+  console.log(req);
+})
 
-        voiceRequest.on('response', function(_response) {
-            response.end(JSON.stringify(_response));
-            // var json = JSON.stringify({'resolvedQuery': _response['result']['resolvedQuery']})
-            // response.end(json);
-        });
+appServer.listen(process.env.PORT || PORT);
 
-        voiceRequest.on('error', function(error) {
-            console.log(error);
-            response.end();
-        });
 
-        request.on('data', function(chunk) {
-            voiceRequest.write(chunk);
-        });
-
-        request.on('end', function() {
-            voiceRequest.end();
-        });
-    } else {
-        response.writeHead(code, {});
-        response.end();
-    }
-
-    console.log(request.headers);
-});
-
-server.listen(process.env.PORT || PORT);
+// var server = http.createServer(function(request, response) {
+//     if (request.method = 'POST' && request.url == '/upload') {
+//         // var outStream = fs.createWriteStream('qwe.wav');
+//         var voiceRequest = app.voiceRequest();
+//
+//         voiceRequest.on('response', function(_response) {
+//             response.end(JSON.stringify(_response));
+//             // var json = JSON.stringify({'resolvedQuery': _response['result']['resolvedQuery']})
+//             // response.end(json);
+//         });
+//
+//         voiceRequest.on('error', function(error) {
+//             console.log(error);
+//             response.end();
+//         });
+//
+//         request.on('data', function(chunk) {
+//             voiceRequest.write(chunk);
+//         });
+//
+//         request.on('end', function() {
+//             voiceRequest.end();
+//         });
+//     } else {
+//         response.writeHead(code, {});
+//         response.end();
+//     }
+//
+//     console.log(request.headers);
+// });
 
 // cat ann_smith.wav | curl -v -X POST --data-binary @- -H "Transfer-Encoding: chunked" -H "Content-Type: audio/wav" http://localhost:8000/upload
