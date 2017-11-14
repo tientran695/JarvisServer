e76145718ff1338b890fcf4e4f91f4326c4952b0
Tien Tran <tientran695@gmail.com>
2017-11-15 01:10:31 +0700

    make it better

---
+++ image.js
@@ -0,0 +1,40 @@
+var express = require("express");
+var app = express();
+var server = require("http").createServer(app);
+var io = require("socket.io").listen(server);
+var fs = require("fs");
+server.listen(process.env.PORT || 3000);
+
+//Mang luu dia chi cac file
+var arrayImage = new Array();
+fs.readdir("image/", function(err, files){
+  if(err){
+    return;
+  }else{
+    files.forEach(function(f){
+      arrayImage.push("image/" + f)
+    })
+  }
+})
+
+io.sockets.on('connection', function(client){
+  console.log("Co nguoi ket noi!");
+  client.on('GETIMAGE', function(data){
+    fs.readFile(getRandomFileName(arrayImage), function(err, data){
+      if(!err){
+        io.emit('SERVER',data);
+        console.log('Da gui hinh');
+      }else{
+        console.log('That bai');
+      }
+    })
+  })
+  client.on('Hello', function(data){
+    console.log("hay lam");
+  })
+})
+
+//Ham random mot phan tu trong mang
+function getRandomFileName(array){
+  return array[Math.floor(Math.random()*array.length)];
+}
