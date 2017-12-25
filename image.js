65e1d5ab55b194ba7d392a1ba1435d40858976ea
= <=>
2017-12-25 16:31:57 +0700

    project 3

--- image.js
+++ image.js
@@ -3,7 +3,31 @@ var app = express();
 var server = require("http").createServer(app);
 var io = require("socket.io").listen(server);
 var fs = require("fs");
+var mongoose = require('mongoose');
 server.listen(process.env.PORT || 3000);
+
+//CSDL MogoDB
+let options = {
+  db: {native_parser: true},
+  server: {poolSize: 5},
+  user: 'tientran695',
+  pass: 'familyofme36'
+};
+var mongoUri = 'mongodb://tientran695:familyofme36@ds141175.mlab.com:41175/iot';
+mongoose.Promise = global.Promise;
+mongoose.connect(mongoUri, options).then(
+  () => {
+    console.log("Connect DB successfully");
+  },
+  err => {
+    console.log('Connect failed. Error: ${err}');
+  }
+);
+var userSchema = new mongoose.Schema({
+  username: String,
+  pass: String
+});
+var db = mongoose.model('users', userSchema);
 
 //Mang luu dia chi cac file
 var arrayImage = new Array();
@@ -23,13 +47,26 @@ io.sockets.on('connection', function(client){
   client.on('GETIMAGE', function(data){
     fs.readFile(getRandomFileName(arrayImage), function(err, data){
       if(!err){
-        io.emit('SERVER',data);
+        io.emit('SERVER SEND IMAGE',data);
         console.log('Da gui hinh');
       }else{
         console.log('That bai');
       }
     })
   })
+
+  client.on("LOGIN", function(user){
+    let name = user.username;
+    let pass = user.pass;
+    db.find({username : name, pass : pass}, function(err, user){
+      if(user.length == 1){
+        io.emit("SERVER SEND LOGIN", {isLogin : true});
+      }else{
+        io.emit("SERVER SEND LOGIN", {isLogin : false});
+      }
+    })
+  })
+
 })
 
 //Ham random mot phan tu trong mang
--- index.js
+++ index.js
@@ -1,9 +1,10 @@
+var PORT = 3000;
 var express = require('express');
 const bodyParser = require('body-parser');
+var mongoose = require('mongoose');
 var appServer = express();
 
-//mongodb
-var mongoose = require('mongoose');
+
 let options = {
   db: {native_parser: true},
   server: {poolSize: 5},
@@ -19,42 +20,33 @@ mongoose.connect(mongoUri, options).then(
   err => {
     console.log('Connect failed. Error: ${err}');
   }
-)
-
-const userSchema = new mongoose.Schema({
-  device: String,
-  location: String,
-  state: String,
-  date: {
-    type: Date,
-    default: Date.now
-  }
+);
+var userSchema = new mongoose.Schema({
+  username: String,
+  pass: String
+});
+var db = mongoose.model('users', userSchema);
+//db.create({username: "tientran695", pass: "123456"});
+// db.find().exec((err, users) => {
+//   console.log(users);
+// })
+var name = "tientran695";
+var pass = "123456";
+db.find({username : name, pass : pass}, function(err, user){
+  //console.log(err);
+  console.log(user);
 })
-
-const db = mongoose.model('Status', userSchema)
-//-----------------------------------------------------
-
 
 appServer.use(bodyParser.urlencoded({
     extended: true
 }));
-
 appServer.use(bodyParser.json());
-
 appServer.post('/echo', function(req, res) {
     var device = req.body.result.parameters.device;
     var location = req.body.result.parameters.location;
     var state = req.body.result.parameters.state;
     //update on mongodb
-    db.create({
-      device: device,
-      location: location,
-      state: state
-    })
-
-    console.log(device);
-    console.log(location);
-    console.log(state);
+    //.................
 });
 
-appServer.listen(process.env.PORT);
+appServer.listen(process.env.PORT || PORT);
